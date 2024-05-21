527375e852def18c8574e061f97409f13ca1c6ac
Hector Espinoza <hector_espinoza02@hotmail.com>
2024-05-21 01:30:51 -0700

    test windows

--- app.py
+++ app.py
@@ -3,9 +3,13 @@ from util import supabase_client
 
 app = Flask(__name__)
 
+@app.context_processor
+def inject_user():
+    return dict(current_user=supabase_client.auth.get_user())
 
 @app.route('/')
 def index():
+    print(supabase_client.auth.get_user())
     return render_template('index.html')
 
 @app.route('/login', methods=["POST", "GET"])
@@ -14,7 +18,13 @@ def login():
         email = request.form['email']
         password = request.form['password']
         print(f"Logging in user {email}...")
-        supabase_client.auth.sign_in_with_password(email, password)
+        print(f"supabase_client.auth.sign_in_with_password({email}, {password})")
+        try:
+            supabase_client.auth.sign_in_with_password({'email': email, 'password': password})
+        except:
+            print(f"Failed to log in user {email}!")
+            return redirect('/login')
+        
         print(f"User {email} logged in successfully!")
         return redirect('/')
 
@@ -32,6 +42,12 @@ def register():
         print(f"User {email} registered successfully!")
         return redirect('/')
     return render_template('register.html')
+
+@app.route('/logout')
+def logout():
+    print("Logging out user...")
+    supabase_client.auth.sign_out()
+    return redirect('/')
 
 @app.route('/arjs')
 def arjs():
